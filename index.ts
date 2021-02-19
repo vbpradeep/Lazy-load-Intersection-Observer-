f8336e1bb39b4412a6096b7d82ccea8110d6eef8
vbpradeep <vbpradeep@live.com>
2021-02-19 23:54:15 +0530

    Initial commit

---
+++ index.ts
@@ -0,0 +1,34 @@
+// Import stylesheets
+import "./style.css";
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById("app");
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+const observe = (entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > 0) {
+      const img = entry.target;
+      const src = img.dataset.src;
+      const srcset = img.dataset.srcset;
+
+      requestIdleCallback(() => {
+        if (srcset) {
+          img.srcset = srcset;
+        }
+        img.src = src;
+      });
+      observer.unobserve(img);
+    }
+  });
+};
+
+const observer = new IntersectionObserver(observe, {
+  root: document.querySelector("body"),
+  rootMargin: "0px",
+  threshold: 1.0
+});
+const images = document.querySelectorAll("img.lazy");
+images.forEach(img => {
+  observer.observe(img);
+});
